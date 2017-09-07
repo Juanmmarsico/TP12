6780244cb554aaf0520cd6788728382c6a526cf9
Ornella <ornellabelloni@gmail.com>
2017-09-06 21:47:47 -0300

    prueba2

--- TP12/src/main/control/action/AboutAction.java
+++ TP12/src/main/control/action/AboutAction.java
@@ -5,14 +5,13 @@ import java.awt.event.ActionEvent;
 import main.GUI.aboutWindow.AboutWindow;
 import main.control.ExpenseManager;
 
-// hola
+// hello
 public class AboutAction extends ExpenseManagerAction {
 
 	public AboutAction(ExpenseManager expenseManager) {
         super(expenseManager);
     }
 
-	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		new AboutWindow(expenseManager);
